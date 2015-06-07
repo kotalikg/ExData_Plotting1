06ea0bc315505ab145f16d1abe3256de86fe52c1
Gabor Kotalik <kotalik.gabor@gmail.com>
2015-06-07 10:47:42 +0200

    Project1

--- plot4.R
+++ plot4.R
@@ -26,5 +26,5 @@ lines(plotbase$Time, plotbase$Sub_metering_3, type = "l", col = "blue")
 legend(x = max(plotbase$Time)-110000, y= max(plotbase$Sub_metering_1)+2, cex = 0.7, lty = 1, bty = "n", col = c("black","blue","red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3") )
 with(plotbase, plot(Voltage ~ Time, type = "l", xlab = "datetime", ylab = "Voltage", bg = "transparent", cex.lab = 0.75, cex.axis = 0.75))
 with(plotbase, plot(Global_reactive_power ~ Time, type = "l", xlab = "datetime", ylab = "Global_reactive_power", cex.lab = 0.75, cex.axis = 0.75))
-dev.copy(png, file = "plot4.png", width = 480, height = 480) #kirakja png-be
+dev.copy(png, file = "plot4.png", width = 480, height = 480)
 dev.off()
